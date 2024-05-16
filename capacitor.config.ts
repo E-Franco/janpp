640d11e86fb55908355dcb0f7b3121bd477a0f24
Eliton Franco <elitonfranco.e@gmail.com>
2024-05-16 15:38:58 -0300

    Update project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'doc24app.janpp.com.br',
+  appId: 'janppsaude.janpp.com.br',
   appName: 'JANPP SAUDE',
   webDir: 'www',
   server: {
